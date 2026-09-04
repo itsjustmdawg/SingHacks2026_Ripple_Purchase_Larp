72324ceb4754bdd9d47eab38acf219e44de958e0
itsjustmdawg <maheshauthayakumar33@gmail.com>
2026-09-05 00:11:59 +0800

    agent wallet management and allow wallet to be displayed in the frontend

--- src/lib/xrpl/index.ts
+++ src/lib/xrpl/index.ts
@@ -1,4 +1,11 @@
 export type { XrplClientConfig, XrplClientService } from "./client";
 export { getXrplClient, disconnectXrplClient, xrplService } from "./client";
+export type { WalletInfo } from "./wallet";
+export {
+  loadWalletFromEnv,
+  getActiveWallet,
+  setActiveWallet,
+  getWalletInfo,
+} from "./wallet";
 export { submitPayment } from "./payment";
 export { verifyTransaction } from "./verify";
